55faa0eebe51b2de70dbcd6c6409f85b4ecfdf7d
DashaShabalina <67578446+DashaShabalina@users.noreply.github.com>
2021-11-16 16:32:31 +0700

    Update ProgramTest.cs

--- test/Calculator.Tests/ProgramTest.cs
+++ test/Calculator.Tests/ProgramTest.cs
@@ -46,7 +46,22 @@ namespace Calculator.Tests
             {
                 result[i] = Regex.Replace(result[i], @"[\r\t\n]+", string.Empty);
             }
-            Assert.Equal(expectedResult, result);
+            
+            Assert.Equal("Console Calculator in C#", result[0]);
+            Assert.Equal("------------------------", result[1]);
+            Assert.Equal("Type a number, and then press Enter: ", result[2]);
+            Assert.Equal("This is not valid input. Please enter an integer value: ", result[3]);
+            Assert.Equal("Type another number, and then press Enter: ", result[4]);
+            Assert.Equal("This is not valid input. Please enter an integer value: ", result[5]);
+            Assert.Equal("Choose an operator from the following list:", result[6]);
+            Assert.Equal("a - Add", result[7]);
+            Assert.Equal("s - Subtract", result[8]);
+            Assert.Equal("m - Multiply", result[9]);
+            Assert.Equal("d - Divide", result[10]);
+            Assert.Equal("Your option? Your result: 8", result[11]);
+            Assert.Equal("------------------------", result[12]);
+            Assert.Equal("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ", result[13]);
+           // Assert.Equal(expectedResult, result);
         }
     }
 }
